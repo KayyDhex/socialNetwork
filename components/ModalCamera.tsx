2fae01ef24c385b0611d41cdaf4d004ba657c720
Desarrollo Tabsa <hans.correa@hackmonkeys.com>
2024-09-17 18:00:34 -0500

    add take photo

--- components/ModalCamera.tsx
+++ components/ModalCamera.tsx
@@ -1,6 +1,6 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { CameraView, CameraType, useCameraPermissions, CameraViewRef } from 'expo-camera';
 import { View, Text, Modal, StyleSheet, Button, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 export interface ModalCameraProps {
     isVisible: boolean,
@@ -15,7 +15,21 @@ export default function ModalCamera({
 }: ModalCameraProps) {
 
     const [facing, setFacing] = useState<CameraType>('back');
+    const cameraRef = useRef<any>()
     const [permission, requestPermission] = useCameraPermissions();
+
+    const handleTakePhoto = async () => {
+        const photo = await cameraRef.current.takePictureAsync({
+            quality: 0.5,
+            base64: true
+        });
+
+        onSave(photo);
+        onClose();
+        console.log({
+            photo
+        })
+    }
 
     return (
         <Modal
@@ -30,10 +44,17 @@ export default function ModalCamera({
                     </View>
                     :
                     <View style={styles.container}>
-                        <CameraView style={styles.camera} facing={facing}>
+                        <CameraView
+                            ref={cameraRef}
+                            style={styles.camera}
+                            facing={facing}
+                        >
                             <View style={styles.buttonContainer}>
                                 <TouchableOpacity style={styles.button} onPress={() => setFacing(prev => prev == 'front' ? 'back' : 'front')}>
                                     <Text style={styles.text}>Flip Camera</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+                                    <Text style={styles.text}>Foto</Text>
                                 </TouchableOpacity>
                                 <TouchableOpacity style={styles.button} onPress={onClose}>
                                     <Text style={styles.text}>Cerrar</Text>
